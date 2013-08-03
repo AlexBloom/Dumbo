3bb9a1d2226468253d60f88570d1cbffa28a8e9b
Alex Bloom <alexbloomdesign@gmail.com>
2013-08-03 10:59:00 -0600

    Stickem Working for Headers & Logos

--- js/main.js
+++ js/main.js
@@ -1,33 +1,4 @@
 $(document).ready(function() {
-	// $('.stickyheader').waypoint('sticky', {
-// 	  wrapper: '<div class="sticky-wrapper" />',
-// 	  stuckClass: 'stuck',
-// 	  offset:50
-// 	});
-	
-	// $('.stickylogo').waypoint('sticky', {
-// 	  wrapper: '<div class="sticky-wrapper" />',
-// 	  stuckClass: 'stuck',
-// 	  offset:50
-// 	});
-	
-	//$('.stickylogo').waypoint('sticky');
-	
-
-
-	$('.sticky-wrapper').waypoint(
-		   function(direction) {   
-		     /* This div is level with the top */  
-		     if(direction == 'down'){
-			   // console.log($(this), '0')
-			     $(this).find('.stickyheader').addClass('stuck');
-		     } 
-		       else $(this).find('.stickyheader').removeClass('stuck');
-		   },
-		   {
-		     offset: 0
-		   }
-		 );   
 
 	
 });
